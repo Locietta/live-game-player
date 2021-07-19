8a30882091df795d668ec93be4f33c8f02ba67fa
DH-Cang <cdh573885@outlook.com>
2021-07-19 12:19:16 +0800

    main entry modified in different system

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include "app.h"
-
 using namespace std;
 
-int main(int, char **) {
+#ifdef WIN32
+#include <windows.h>
+
+int main(int argc, char* argv[]) {
+    FreeConsole();
     App a;
     bool t = a.init();
     return a.run();
 }
+
+#else
+
+int main(int argc, char* argv[]) {
+    App a;
+    bool t = a.init();
+    return a.run();
+}
+
+#endif
